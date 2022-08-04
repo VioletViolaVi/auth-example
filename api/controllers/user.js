9a24b012c5332613a9ace4cd3dbbbf07bd82105e
vivian <vivian.v18723@gmail.com>
2022-08-04 17:49:40 +0100

    add try catch block

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,19 +1,31 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 async function login(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
 
-  const user = await User.getOneByUsername(username);
+    const user = await User.getOneByUsername(username);
 
-  // check if password matches hash
+    // Check here if the password matches the hash
+    const authenticated = await bcrypt.compare(password, user.password);
 
-  console.log(username, password);
-  console.log(user);
+    if (authenticated) {
+      res.json({
+        success: true
+      });
+    } else {
+      throw "Credentials didn't match.";
+    }
+  } catch (err) {
+    console.log(err);
 
-  res.json({
-    success: true
-  });
+    res.json({
+      success: false,
+      error: "Unable to authenticate user."
+    });
+  }
 }
 
 module.exports = { login };
