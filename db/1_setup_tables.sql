4c8405f801c8bb76fa9012dd7a220d9c06fee79b
vivian <vivian.v18723@gmail.com>
2022-08-04 16:47:31 +0100

    create new sql table

--- db/1_setup_tables.sql
+++ db/1_setup_tables.sql
@@ -1,5 +1,13 @@
-CREATE TABLE post (
+CREATE TABLE
+  post (
     id serial PRIMARY KEY,
     title varchar(100),
     text varchar(500)
-)
+  );
+
+CREATE TABLE
+  user_account (
+    id serial PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    password CHAR(60) NOT NULL
+  );
