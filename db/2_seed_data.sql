9caa556d5ca855b611965411487e9c9a594da6e7
vivian <vivian.v18723@gmail.com>
2022-08-04 17:01:16 +0100

    refactor code

--- db/2_seed_data.sql
+++ db/2_seed_data.sql
@@ -1,14 +1,15 @@
-INSERT INTO post (
-    title,
-    text
-) VALUES (
-    'Welcome!', 'This is a highly-exclusive social network, open only to the elite.'
-);
+INSERT INTO
+  post (title, text)
+VALUES
+  (
+    'Welcome!',
+    'This is a highly-exclusive social network, open only to the elite.'
+  );
 
-INSERT INTO user_account (
-    username,
-    password
-) VALUES (
+INSERT INTO
+  user_account (username, password)
+VALUES
+  (
     'admin',
     '$2y$12$i3cMrFBKZbM2lr/zSfkxA.7stI.xvGhcdwkDoAJKYmYr.X0rQ.MOm'
-);
+  );
