a352a1ce164f8af10b3f534352732a1c02a497f8
vivian <vivian.v18723@gmail.com>
2022-08-11 18:10:56 +0100

    create options for both fetch requests

--- client/public/assets/scripts/index.js
+++ client/public/assets/scripts/index.js
@@ -1,63 +1,76 @@
 const createPostElement = (data) => {
+  const elem = document.createElement("div");
+  const heading = document.createElement("h2");
+  const paragraph = document.createElement("p");
 
-    const elem = document.createElement("div");
-    const heading = document.createElement("h2");
-    const paragraph = document.createElement("p");
+  heading.textContent = data["title"];
+  paragraph.textContent = data["text"];
 
-    heading.textContent = data["title"];
-    paragraph.textContent = data["text"];
+  elem.append(heading, paragraph);
 
-    elem.append(heading, paragraph);
-
-    return elem;
-}
+  return elem;
+};
 
 const loadPosts = () => {
+  const container = document.querySelector("container");
 
-    const container = document.querySelector("container");
+  const options = {
+    headers: {
+      Authorization: new Headers({
+        Authorization: localStorage.getItem("token")
+      })
+    }
+  };
 
-    fetch("http://localhost:3000/posts")
-        .then(res => res.json())
-        .then(data => {
-            data["posts"].forEach(p => {
-                const elem = createPostElement(p);
-                container.appendChild(elem);
-            })
-        })
-}
+  fetch("http://localhost:3000/posts", options)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data["success"]) {
+        data["posts"].forEach((p) => {
+          const elem = createPostElement(p);
+          container.appendChild(elem);
+        });
+      } else {
+        throw "Unauthorised access!";
+      }
+    })
+    .catch((err) => {
+      window.location.assign("login_register");
+    });
+};
 
 function createPost(data) {
-
-    const options = {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
     }
+  };
 
-    fetch("http://localhost:3000/posts", options)
-        .then(res => res.json())
-        .then(data => {
-            document.querySelector("container").appendChild(createPostElement(data["post"]));
-        })
-        .catch(err => alert(err))
+  fetch("http://localhost:3000/posts", options)
+    .then((res) => res.json())
+    .then((data) => {
+      document
+        .querySelector("container")
+        .appendChild(createPostElement(data["post"]));
+    })
+    .catch((err) => alert(err));
 }
 
 // Connect everything to the page
 
-document.querySelector('#post-form').addEventListener("submit", (e) => {
-    e.preventDefault();
+document.querySelector("#post-form").addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const form = new FormData(e.target);
+  const form = new FormData(e.target);
 
-    createPost({
-        title: form.get("title"),
-        text: form.get("text")
-    })
+  createPost({
+    title: form.get("title"),
+    text: form.get("text")
+  });
 
-    e.target.reset();
-
-})
+  e.target.reset();
+});
 
 loadPosts();
